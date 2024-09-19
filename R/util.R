492d0504d9f6176cd211d995a7d37f04bed1ea5d
Carolina Euán <carolina.euan.campos@gmail.com>
2024-09-19 15:51:58 +0100

    PFF documentation

--- R/util.R
+++ R/util.R
@@ -46,7 +46,7 @@ clr2density <- function(z, z_step, clr)
   if(fda::is.fd(clr)) {
     return(exp(eval.fd(z, clr)) / as.vector(trapzc(z_step, exp(eval.fd(z, clr)))))
   } else {
-    return(exp(clr) / trapzc(z_step, exp(clr)))
+    return(exp(clr) / as.vector(trapzc(z_step, exp(clr))))
   }
 }
 
@@ -62,86 +62,6 @@ clr2density <- function(z, z_step, clr)
 #' @export
 trapzc <- function(step, y) {
   step * t(y) %*% c(0.5, rep(1, length(y) - 2), 0.5)
-}
-
-
-
-
-############factor_to_sources###################################################
-#' factor_to_sources
-#'
-#' After fitting a factor model (output from FDF) it transforms factors and 
-#' contributions to original density space
-#' 
-#' @param factor_hat  Output from FDF.R 
-#' @param dates Data dates
-#' @param CLR_data  Output from PNSDdata_fd
-#' @param fd_data   Output from PNSDdata_fd
-#'
-#' @return list()
-#'   SourceProfiles: densities 
-#'   SourceContributions: time series contributions 
-#'   and other metadata
-#'   
-#' @export
-#'
-#' @examples 
-#' see document 
-factor_to_sources <- function(factor_hat, dates, CLR_data, fd_data,flip=TRUE){
-
-  # log.x new sequence but can be anything!
-  log.x <- CLR_data$x.fine
-  
-  # num of selected factors
-  kbar <- dim(factor_hat$hat.F$coefs)[2]   # ****** THIS IS A PROBLEM, NOT DEFINED
-
-  # Transform to Bayes space of factors
-  hat.fM <- matrix(0, nrow = length(log.x), ncol = kbar)
-  for (i in 1:kbar) {
-    hat.fM[, i] = clr2density(log.x, z_step = CLR_data$x.step,
-                              factor_hat$hat.F[i, ])
-  }
-  
-  # Define estimated factors as Source profiles
-  SourceProfiles <- hat.fM
-  
-  # define SourceContributions as the corresponding scores
-  SourceContributions <-
-    data.frame(dates, factor_hat$hat.beta[, 1:kbar])
-  names(SourceContributions) <- c("time", paste0('Factor', 1:kbar))
-  
-  
-  if(flip){
-  flip.out <- run_flip(SourceProfiles, SourceContributions, log.x)
-  }else{
-    SourceProfiles<-data.frame(SourceProfiles)
-    names(SourceProfiles) <- c(paste0('Factor', 1:kbar))
-    flip.out <-list(
-      SourceProfiles = SourceProfiles,
-      SourceContributions = SourceContributions,
-      signs = rep(1,kbar),
-      sig2 = NULL
-    )
-  }
-  
-  betatilde <- TS.contribution(flip.out$SourceProfiles,
-                               flip.out$SourceContributions,
-                               fd_data,
-                               CLR_data,
-                               log.x)
-
-  return(
-    list(
-      SourceProfiles = flip.out$SourceProfiles,
-      SourceContributions = betatilde$SourceContributions3,
-      SourceContributions0 = flip.out$SourceContributions,
-      RowSumSourceContribution = betatilde$RowSumSourceContribution,
-      log.x = log.x,
-      signs = flip.out$signs,
-      sig2 = flip.out$sig2,
-      x.step = CLR_data$x.step
-    )
-  )
 }
 
 ############run_flip############################################################
