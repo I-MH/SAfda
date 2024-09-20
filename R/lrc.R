5dc2fba83a6fff9e9de1072fd1e515b8e770256b
Israel Martinez Hernandez <israel.m.hdz@gmail.com>
2024-09-20 16:10:13 +0100

    lrc documentation

--- R/lrc.R
+++ R/lrc.R
@@ -1,166 +1,29 @@
-#' lrc Long-Run Covariance
-#'
-#' Estimates the long-run covariance operator, as discussed originally in
+#' Estimation of the Lambda operator - Long-run covariance
+#' 
+#' Estimates the long-run covariance operator lambda described in the main paper. 
+#' 
+#' This uses the method discussed originally in
 #' Rice, G., and Shang, H.L. (2017), "A Plug-in Bandwidth Selection Procedure for Long-run 
 #' Covariance Estimation with Stationary Functional Time Series", 
 #' Journal of Time Series Analysis, 38, 591–609.
 #'
-#' @param datafd Input data, in functional data object.
-#' @param kern_type Kernel type, switched internally to \code{c("BT", "PR", "SP", "FT", "flat")} which are Bartlett, Parzen, Simple, flat_top or flat kernels.
-#' @param h The bandwidth parameter, strictly non-zero.
-#'
-#' @importFrom fda fd
-#' @importFrom fda pca.fd
-#' @importFrom fda is.fd
-#'
-#' @return list
-#' @export
-#'
-#' @examples 
-lrc <- function(datafd, kern_type, h) {
-
-  stopifnot(fda::is.fd(datafd), is.character(kern_type),
-            kern_type %in% c("BT", "PR", "SP", "FT", "flat"),
-            is.numeric(h), h > 0)
-  
-  kerneltype <- switch(
-    kern_type,
-    BT = "Bartlett",
-    PR = "Parzen",
-    FT = "flat_top",
-    SP = "Simple",
-    flat = "flat"
-  )
-
-  N <- dim(datafd$coefs)[2]
-  nbasis <- datafd$basis$nbasis
-  CCh0 <- datafd$coefs[, 1:N] %*% t(datafd$coefs[, 1:N])
-  CCh <- matrix(0, nbasis, nbasis) + CCh0
-  
-  for (j in 1:h) {
-    AuxCCh <- datafd$coefs[, (1 + j):N] %*% t(datafd$coefs[, 1:(N - j)]) +
-      t(datafd$coefs[, (1 + j):N] %*%  t(datafd$coefs[, 1:(N - j)]))
-    CCh <- CCh + Kernel(j, h, kerneltype) * AuxCCh
-  }
-  # for the case where it is not symmetric
-  CCh <- (CCh + t(CCh)) / 2 
-
-  xbasis <- datafd$basis
-  Jinprod <- inprod(xbasis, xbasis)
-  Jaux <- eigen(Jinprod)
-  Jhalf <- Jaux$vectors %*% diag(sqrt(Jaux$values)) %*% t(Jaux$vectors)
-  Jihalf <- solve(Jhalf)
-  Ob <- (1 / N) * Jhalf %*% CCh %*% Jhalf
-  result <- eigen(Ob)
-  
-  # coefficients of the eigenfunctions
-  bb <- Jihalf %*% Re(result$vectors) 
-  
-  # eigenvalues of the operator
-  eigenval <- Re(result$values) 
-  eigenf <- fda::fd(Re(bb), basisobj = xbasis)
-  
-  return(list(
-    lrc.k = CCh,
-    Ob = Ob,
-    bb = Re(bb),
-    eigenf = eigenf,
-    eigenval = eigenval
-  ))
-}
-
-#' lrc_paper Long-Run Covariance, Variant 2
-#' 
-#' Estimates the long-run covariance, as discussed originally in 
-#' [Israel's paper]
-#'
-#' @param datafd Input data, in FD format.
-#' @param kern_type Kernel type, switched internally to \code{c("BT", "PR", "SP", "FT", "flat")} which are Bartlett, Parzen, Simple, flat_top or flat kernels.
-#' @param h The bandwidth parameter, strictly non-zero.
-#' @param p fill in
-#'
-#' @return list
-#' @export
-#'
-#' @importFrom fda fd
-#' @importFrom fda is.fd
-#'
-#' @examples rnorm(1)
-lrc_paper <- function(datafd, kern_type,h,p) {
-
-  stopifnot(fda::is.fd(datafd), is.character(kern_type),
-            kern_type %in% c("BT", "PR", "SP", "FT", "flat"),
-            is.numeric(h), h > 0)
-  
-  # returns the eigenfunctions of the long run cov
-  kerneltype <- switch(
-    kern_type,
-    BT = "Bartlett",
-    PR = "Parzen",
-    FT = "flat_top",
-    SP = "Simple",
-    flat = "flat"
-  )
-
-  N <- dim(datafd$coefs)[2]
-  nbasis <- datafd$basis$nbasis
-
-  Datopca <- fda::pca.fd(datafd, nharm = p)
-  lam0 <- Datopca$values
-  bb0 <- Datopca$harmonics$coefs   # nbasis x num of factors
-  BB <- (1 / lam0[1]) * bb0[, 1] %*% t(bb0[, 1])
-  for (l in 2:p) {
-    BB <- BB + (1 / lam0[l]) * bb0[, l] %*% t(bb0[, l])
-  }
-  
-  CCh <- matrix(0, nbasis, nbasis)
-  for (j in 1:h) {
-    AuxCCh <- datafd$coefs[, (1 + j):N] %*%  t(datafd$coefs[, 1:(N - j)]) +
-      t(datafd$coefs[, (1 + j):N] %*%  t(datafd$coefs[, 1:(N - j)]))
-    CCh <- CCh + Kernel(j, h, kerneltype) * AuxCCh
-  }
-  CCh <- (CCh + t(CCh)) / 2 # in case it is not symmetric
-
-  xbasis <- datafd$basis
-  Jinprod <- inprod(xbasis, xbasis)
-  Jaux <- eigen(Jinprod)
-  Jhalf <- Jaux$vectors %*% diag(sqrt(Jaux$values)) %*% t(Jaux$vectors)
-  Jihalf <- solve(Jhalf)
-  Ob <- (1 / N) * Jhalf %*% CCh %*% Jinprod %*% BB %*% Jhalf
-  result <- eigen(Ob)
-  
-  # coefficients of the eigenfunctions
-  bb <- Jihalf %*% Re(result$vectors) 
-  # eigenvalues of the operator
-  eigenval <- Re(result$values) 
-  eigenf <- fda::fd(Re(bb), basisobj = xbasis)
-  
-  return(list(
-    lrc.k = CCh,
-    Ob = Ob,
-    bb = Re(bb),
-    eigenf = eigenf,
-    eigenval = eigenval
-  ))
-}
-
-#' lrc_paper_NoInv Long-Run Covariance, Variant 2, No Inverse
-#'
-#' Estimates the long-run covariance, as discussed originally in 
-#' [Israel's paper]. This is the variant without the inverse.
 #'
 #' @param datafd Input data, in FD format.
 #' @param kern_type Kernel type, switched internally to \code{c("BT", "PR", "SP", "FT", "flat")} which are Bartlett, Parzen, Simple, flat_top or flat kernels.
 #' @param h The bandwidth parameter, strictly non-zero.
 #' @param replicates Number of replicates (default NULL) for the ...
 #'
-#' @return list
+#' @return 
+#' A list with:
+#' \itemize{
+#'   \item \code{eigenf} - eigenfunctions of the operator
+#'   \item \code{eigenval} - eigenvalues of the operator
+#'   \item \code{lrc.k} - kernel of the lambda operator
+#' }
 #' @export
 #'
 #' @importFrom fda fd
 #' @importFrom fda is.fd
-#'
-#' @examples rnorm(1)
 lrc_paper_NoInv <- function(datafd, 
                             kern_type,
                             h, 
